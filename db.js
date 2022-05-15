1761674cd52a6bee61155628484906b7108aafaf
Brian Shapiro <briandavidshapiro@gmail.com>
2022-05-15 17:05:56 -0400

    setup db

--- db.js
+++ db.js
@@ -3,12 +3,17 @@ const mysql = require("mysql2");
 const { DB_URI } = require("./config");
 
 var db = mysql.createConnection({
-	host: "localhost",
-	port: 8889,
-	user: "root",
-	password: "potentiate",
-	database: "ghnk",
+	host: process.env.DATABASE_HOST || "localhost",
+	port: 3306,
+	user: process.env.DATABASE_USERNAME || "root",
+	password: process.env.DATABASE_PASSWORD || "potentiate",
+	database: process.env.DATABASE_NAME || "ghnk",
 });
+
+console.log("process.env.DATABASE_HOST = ", process.env.DATABASE_HOST);
+console.log("process.env.DATABASE_USERNAME = ", process.env.DATABASE_USERNAME);
+console.log("process.env.DATABASE_PASSWORD = ", process.env.DATABASE_PASSWORD);
+console.log("process.env.DATABASE_NAME = ", process.env.DATABASE_NAME);
 
 db.connect(function (err) {
 	if (err) throw err;
