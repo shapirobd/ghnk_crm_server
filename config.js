93dfc08b22e1d15c632663a4d09e5cdb8a0cadcf
Brian Shapiro <briandavidshapiro@gmail.com>
2022-05-15 16:14:40 -0400

    heroku stuff

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ let DB_URI = `postgresql://`;
 if (process.env.NODE_ENV === "test") {
 	DB_URI = `${DB_URI}/ghnk_test`;
 } else {
-	DB_URI = process.env.DATABASE_URI || `${DB_URI}/ghnk`;
+	DB_URI = process.env.DATABASE_URL || `${DB_URI}/ghnk`;
 }
 
 const BCRYPT_WORK_FACTOR = 12;
