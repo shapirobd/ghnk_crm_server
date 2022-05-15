5e24ff11d79800b2801ff495008a0f4bcf1d0da5
Brian Shapiro <briandavidshapiro@gmail.com>
2022-05-15 17:11:03 -0400

    test

--- config.js
+++ config.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV === "test") {
 	DB_URI = process.env.DATABASE_URL || `${DB_URI}/ghnk`;
 }
 
+console.log(DB_URI);
+
 const BCRYPT_WORK_FACTOR = 12;
 const SECRET_KEY = process.env.SECRET_KEY;
 module.exports = { DB_URI, BCRYPT_WORK_FACTOR, SECRET_KEY };
